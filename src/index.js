708b0afaf6f7370f241f44750406d669b437a9bf
valeryiaStrom <lerastrom1@gmail.com>
2019-10-03 23:52:27 +0300

    feat: add completed task multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function multiply(first, second) {
   // your solution
+
+  let firstNumber = BigInt(first);
+  let secondNumber = BigInt(second);
+  let product = firstNumber * secondNumber;
+  return product.toString();
 }
